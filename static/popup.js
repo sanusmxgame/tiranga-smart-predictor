649ddd89987905a1ce459618b49b980978b5fb0a
sanusmxgame <sanusmxgame@gmail.com>
2025-07-03 13:56:09 +0530

    Update popup.js

--- static/popup.js
+++ static/popup.js
@@ -1,1 +1,5 @@
-// JS or script content
+(async () => {
+  const res = await fetch('https://your-deployment-url/api/predict');
+  const data = await res.json();
+  alert(`Next color: ${data.likely_color} (${data.confidence}% confidence)`);
+})();
