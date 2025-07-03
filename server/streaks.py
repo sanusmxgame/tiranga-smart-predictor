7d11362b196834cfbc6971a284fa1ebf989b5c56
sanusmxgame <sanusmxgame@gmail.com>
2025-07-03 13:55:11 +0530

    Update streaks.py

--- server/streaks.py
+++ server/streaks.py
@@ -1,1 +1,16 @@
-# streaks.py
+from database import get_history
+
+def analyze_streaks():
+    data = get_history()
+    last_color = None
+    streak = 0
+    max_streak = {"color": None, "count": 0}
+    for d in data:
+        if d["color"] == last_color:
+            streak += 1
+        else:
+            streak = 1
+            last_color = d["color"]
+        if streak > max_streak["count"]:
+            max_streak = {"color": last_color, "count": streak}
+    return max_streak
