cdfa62e6a6767ecb332675b61494bc358f4ab95d
sanusmxgame <sanusmxgame@gmail.com>
2025-07-03 13:54:51 +0530

    Update hash_decoder.py

--- server/hash_decoder.py
+++ server/hash_decoder.py
@@ -1,1 +1,6 @@
-# hash_decoder.py
+def decode_hash(hash_str):
+    # This is placeholder - replace with your SHA256 reverse logic if discovered
+    return {
+        "decoded_number": 7,
+        "color": "Red"
+    }
