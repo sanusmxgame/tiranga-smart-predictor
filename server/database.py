d3e45128bb96162a07dc62d12489b8903e818c3f
sanusmxgame <sanusmxgame@gmail.com>
2025-07-03 15:10:19 +0530

    Update database.py

--- server/database.py
+++ server/database.py
@@ -1,29 +1,17 @@
 import os, json
 from pymongo import MongoClient
 
-USE_MONGO = os.environ.get("mongodb+srv://tirangaUser:yourStrongPassword123@cluster0.xep0rbi.mongodb.net/tiranga") is not None
-if USE_MONGO:
-    client = MongoClient(os.environ["mongodb+srv://tirangaUser:yourStrongPassword123@cluster0.xep0rbi.mongodb.net/tiranga"])
-    db = client["tiranga"]
-    coll = db["history"]
-else:
-    JSON_PATH = "history.json"
-    if not os.path.exists(JSON_PATH):
-        with open(JSON_PATH, 'w') as f: json.dump([], f)
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://tirangaUser:yourStrongPassword123@cluster0.xep0rbi.mongodb.net/tiranga")
+client = MongoClient(MONGO_URI)
+db = client["tiranga"]
+collection = db["results"]
 
 def save_round(round_data):
-    if USE_MONGO:
-        coll.insert_one(round_data)
-    else:
-        with open(JSON_PATH, 'r+') as f:
-            data = json.load(f)
-            data.append(round_data)
-            f.seek(0)
-            json.dump(data[-100:], f)
+    collection.update_one(
+        {"period": round_data["period"], "_source": round_data["_source"]},
+        {"$set": round_data},
+        upsert=True
+    )
 
 def get_history():
-    if USE_MONGO:
-        return list(coll.find({}, {"_id": 0}))[-100:]
-    else:
-        with open(JSON_PATH) as f:
-            return json.load(f)
+    return list(collection.find({}, {"_id": 0}))[-100:]
